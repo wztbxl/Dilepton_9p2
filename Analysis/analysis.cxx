b9006619b18bca7f7d0bf1897420daddd12e1ae8
wztbxl <35267233+wztbxl@users.noreply.github.com>
2023-12-15 23:39:54 +0800

    add the nElectron Dis of each badrun

--- Analysis/analysis.cxx
+++ Analysis/analysis.cxx
@@ -366,7 +366,7 @@ int main(int argc, char** argv)
 		current_nEMinus=0;
 		Int_t npTrks = event->mNTrks;
 		nPi_K_P_tof = event->mnChargeParticle;
-		// cout << "npTrks = " << npTrks << endl;
+		cout << "npTrks = " << npTrks << endl;
 		// nPi_K_P_tof = 0;
 		for(int j=0;j<npTrks;j++) passTrack(event,j); //Trk loop
     	// cout << "after passtrack" << endl;
@@ -374,7 +374,7 @@ int main(int argc, char** argv)
 		mBadRun_nElectron[name]->Fill(current_nEPlus,current_nEMinus);
 		hnEMinusvsEPlus->Fill(current_nEPlus,current_nEMinus);
 		hRefMultvsnPiKP->Fill(event->mRefMult,nPi_K_P_tof);
-		// cout << "nPi_K_P_tof = " << nPi_K_P_tof << endl; 
+		cout << "nPi_K_P_tof = " << nPi_K_P_tof << endl; 
 
 		// if(!nPi_K_P_rejection(event->mRefMult,nPi_K_P_tof))
 		// {
